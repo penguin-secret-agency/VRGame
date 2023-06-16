51b4682debfe2737755e342b93f209cb8f8bd22c
Suriel Gonçalves <surielgoncalves@gmail.com>
2023-06-15 21:07:34 -0300

    Health System

---
+++ 3D Project/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour {
+    public float health = 100;
+    public void addHealth(float amount) {
+        health+=amount;
+    }
+    public void decreasedHealth(float amount) {
+        health-=amount;
+        if(health<=0) {
+            health=0;
+        }
+    }
+}
