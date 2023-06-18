92f9b8d735ca3261bd26fc445994258d1b36840d
Suriel Gonçalves <surielgoncalves@gmail.com>
2023-06-18 02:08:33 -0300

    Update HealthSystem

--- 3D Project/Assets/Scripts/HealthSystem.cs
+++ 3D Project/Assets/Scripts/HealthSystem.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour {
     public float health = 100;
-
+    public GameManagerScript gameManager;
     public void addHealth(float amount) {
         health+=amount;
     }
     public void decreasedHealth(float amount) {
-        health-=amount;
-        if(health<=0) {
+        bool willDie = health>0&&health-amount<=0;
+        if(willDie) {
             health=0;
+            gameManager.GameOver();
+            disablePlayer();
+        } else if(health > 0){
+            health-=amount;
+        }
+    }
+
+    private void disablePlayer() {
+        PlayerLook look = gameObject.GetComponent<PlayerLook>();
+        PlayerMovement movement = gameObject.GetComponent<PlayerMovement>();
+        if(look!= null) {
+            look.enabled=false;
+        }
+        if(movement!=null) {
+            movement.enabled=false;
         }
     }
 
